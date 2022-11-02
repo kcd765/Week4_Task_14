bd5bfffd3c4240251ff6800543ca936ffb944395
Kok Cheng Da <kcd765@gmail.com>
2022-11-02 20:25:25 +0800

    Fixed errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Footer1 } from "./stories/components/Footer/Footer.stories";
 import { Header1 } from './stories/components/Header/Header.stories';
-import { Form1 } from './components/Form/Form.stories'
-import { CardDisplay1 } from './components/CardDisplay/CardDisplay.stories'
+import { Form } from './components/Form/Form'
+import { CardDisplay } from './components/CardDisplay/CardDisplay'
 import './App.css'
 
 const App = () => {
@@ -11,11 +11,11 @@ const App = () => {
       <Header1 backgroundColor="green" />
       <div className="row">
         <div className="column">
-          <Form1 />
+          <Form />
         </div>
 
         <div className="column">
-          <CardDisplay1 />
+          <CardDisplay />
         </div>
       </div>
       <Footer1 color1="#61764B" color2="#9BA17B" height="5%" />
